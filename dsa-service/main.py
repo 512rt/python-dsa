23206b8b10f8fa379d9490c24e5a40a28f4b76db
“512rt” <rohit.developer90@gmail.com>
2025-02-15 14:30:12 -0500

    folder structure

--- dsa-service/main.py
+++ dsa-service/main.py
@@ -1,9 +1,9 @@
 from sorting import sorting
 
 def main():
-    sorting = sorting()
+    sort = sorting()
     arr = [2,1,7,4,3,8,5]
-    print("result -> ", sorting.insertion_sort(arr))
+    print("result -> ", sort.insertion_sort(arr))
 
 
 if __name__ == "__main__":
