eb55de7a0085b5e93a48b807a86e547ac2b908c6
“512rt” <rohit.developer90@gmail.com>
2025-02-15 16:44:17 -0500

    ll add two nums

--- dsa-service/linked_list.py
+++ dsa-service/linked_list.py
@@ -1,5 +1,5 @@
 
-from typing import Optional
+from typing import List, Optional
 
 
 class ListNode:
@@ -14,18 +14,30 @@ class linked_list:
 
     def load_run_ll(self):
         # Set up linked list - load data
-        nodes = [1,2,3,4,5,6]
-        print(f"src -> {nodes}")
+        # nodes = [1,2,3,4,5,6]
+        # print(f"src -> {nodes}")
 
-        dummy = ListNode()
-        cur = dummy
-        for n in nodes:
-            cur.next = ListNode(n)
-            cur = cur.next
+        # dummy = ListNode()
+        # cur = dummy
+        # for n in nodes:
+        #     cur.next = ListNode(n)
+        #     cur = cur.next
         
         # calling the code
-        head = dummy.next
-        result = self.reverseBetween(head, 2, 5)
+        # head = self.load_ll(nodes)
+        # result = self.reverseBetween(head, 2, 5)
+
+        # calling the code
+    #Input: l1 = [2,4,3], l2 = [5,6,4]
+
+        node1 = [1,8,9]
+        node2 = [4,5,7]
+        print(f"l1 -> {node1}")
+        print(f"l2 -> {node2}")
+        head1 = self.load_ll(node1)
+        head2 = self.load_ll(node2)
+        result = self.addTwoNumbers(head1, head2)
+
 
         # Printing the result
         res = []
@@ -35,6 +47,17 @@ class linked_list:
             cur = cur.next
         
         print(f"result -> {res}")
+    
+    def load_ll(self, nodes: List[int]) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+        for n in nodes:
+            cur.next = ListNode(n)
+            cur = cur.next
+        
+        return dummy.next
+        
+
             
 
 
@@ -81,3 +104,47 @@ class linked_list:
 
         return dummy.next
 
+    #region 2. Add Two Numbers
+    #You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    #You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    #Example 1:
+
+    #Input: l1 = [2,4,3], l2 = [5,6,4]
+    #Output: [7,0,8]
+    #Explanation: 342 + 465 = 807.
+    #Example 2:
+
+    #Input: l1 = [0], l2 = [0]
+    #Output: [0]
+    #Example 3:
+
+    #Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    #Output: [8,9,9,9,0,0,0,1]
+    #Constraints:
+    #The number of nodes in each linked list is in the range [1, 100].
+    #0 <= Node.val <= 9
+    #It is guaranteed that the list represents a number that does not have leading zeros.
+    #endregion
+
+    def addTwoNumbers(self, l1, l2):
+        
+        carry = 0
+
+        dummy = ListNode(0)
+        cur = dummy
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            res = v1 + v2 + carry
+            carry = res//10
+            res = res%10
+
+            cur.next = ListNode(res)
+            cur = cur.next
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummy.next
